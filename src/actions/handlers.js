6675f8d01c9894a70a5527ca1e2b97ac4dbdbe5b
Jakub <stawowykuba@gmail.com>
2021-12-05 12:25:08 +0100

    Checkpoint

--- src/actions/handlers.js
+++ src/actions/handlers.js
@@ -32,47 +32,47 @@ export const handleError = (error, history, setLogged) => {
 }
 
 export const sendMessage = (userId, message = "You have received a new message") => {
-
-    let stompClient = null;
-    const newConnect = (onConnect) => {
-        // alert("Podlaczam sie");
-        const socket = new SockJS(BASE_URL + '/ws');
-        stompClient = Stomp.over(socket);
-        stompClient.connect(getConfig(), () => {
-            stompClient.subscribe('/topic/' + userId);
-            onConnect();
-        }, error => alert(error));
-    }
-
-    newConnect(() => {
-        if (stompClient) {
-            stompClient.send('/app/send', getConfig(), JSON.stringify({
-                value: message,
-                token: localStorage.getItem(TOKEN),
-                user: userId
-            }));
-            // alert('sent')
-        }
-        else {
-            alert('stomp client jest null');
-        }
-        // reconnect();
-    });
+    //
+    // let stompClient = null;
+    // const newConnect = (onConnect) => {
+    //     // alert("Podlaczam sie");
+    //     const socket = new SockJS(BASE_URL + '/ws');
+    //     stompClient = Stomp.over(socket);
+    //     stompClient.connect(getConfig(), () => {
+    //         stompClient.subscribe('/topic/' + userId);
+    //         onConnect();
+    //     }, error => alert(error));
+    // }
+    //
+    // newConnect(() => {
+    //     if (stompClient) {
+    //         stompClient.send('/app/send', getConfig(), JSON.stringify({
+    //             value: message,
+    //             token: localStorage.getItem(TOKEN),
+    //             user: userId
+    //         }));
+    //         // alert('sent')
+    //     }
+    //     else {
+    //         alert('stomp client jest null');
+    //     }
+    //     // reconnect();
+    // });
 }
 
 export const reconnect = () => {
-
-    let stompClient = null;
-    const connect = () => {
-        // alert("Lacze sie");
-        const socket = new SockJS(BASE_URL + '/ws');
-        stompClient = Stomp.over(socket);
-        stompClient.connect(getConfig(), onConnected, error => alert(error));
-    }
-
-    const onConnected = () => {
-        stompClient.subscribe('/topic/' + localStorage.getItem('userId'), message => alert(JSON.parse(message.body).value));
-    }
-
-    connect();
+    //
+    // let stompClient = null;
+    // const connect = () => {
+    //     // alert("Lacze sie");
+    //     const socket = new SockJS(BASE_URL + '/ws');
+    //     stompClient = Stomp.over(socket);
+    //     stompClient.connect(getConfig(), onConnected, error => alert(error));
+    // }
+    //
+    // const onConnected = () => {
+    //     stompClient.subscribe('/topic/' + localStorage.getItem('userId'), message => alert(JSON.parse(message.body).value));
+    // }
+    //
+    // connect();
 }
