c84d500ec986962ec4203e3cd3277b01b7c38704
Jakub <stawowykuba@gmail.com>
2021-12-19 14:56:12 +0100

    Heroku release 1.2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import {makeStyles} from "@material-ui/core";
 import {LoginRegister} from "./pages/LoginRegister";
 import {BrowserRouter} from "react-router-dom";
 import {Redirect, Route, Switch} from "react-router";
-import {Home} from "./pages/Home";
-import {DeliveryPage} from "./pages/DeliveryPage";
-import {ProfilePage} from "./pages/ProfilePage";
-import {Messages} from "./pages/Messages";
-import {RegisterOrderPage} from "./pages/RegisterOrderPage";
-import {EditOrderPage} from "./pages/EditOrderPage";
-import {OrderPage} from "./pages/OrderPage";
+// import {Home} from "./pages/Home";
+// import {DeliveryPage} from "./pages/DeliveryPage";
+// import {ProfilePage} from "./pages/ProfilePage";
+// import {Messages} from "./pages/Messages";
+// import {RegisterOrderPage} from "./pages/RegisterOrderPage";
+// import {EditOrderPage} from "./pages/EditOrderPage";
+// import {OrderPage} from "./pages/OrderPage";
 
 import wallpaper from './uploads/wallpaper.png';
 import {useEffect, useState} from "react";
@@ -67,9 +67,10 @@ function App() {
               {/*       component={OrderPage}*/}
               {/*       // connect={connect}*/}
               {/*       render={()=><OrderPage setLogged={setLogged}/>} />*/}
-              {/*<Route exact path={'/'}>*/}
-              {/*    {isLogged() ? <Redirect to={'/home'}/> : <Redirect to={'/login'}/>}*/}
-              {/*</Route>*/}
+              <Route exact path={'/'}>
+                  <Redirect to={'/login'}/>
+                  {/*{isLogged() ? <Redirect to={'/home'}/> : <Redirect to={'/login'}/>}*/}
+              </Route>
               {/*<Route exact path={'/*'}>*/}
               {/*    {isLogged() ? <Redirect to={'/home'}/> : <Redirect to={'/login'}/>}*/}
               {/*</Route>*/}
